ca3d8106298ffa372272e60e0b70a7a6acdcc685
Richard Brown <richard@agilepixel.io>
2020-10-12 21:32:14 +0100

    config and package update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
+/*! 🧮🧩 2020*/
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true,
     },
-    "extends": [
-        "agilepixel"
+    'extends': [
+        'agilepixel',
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly',
     },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    }
+    'parserOptions': {'ecmaVersion': 2018},
 };
